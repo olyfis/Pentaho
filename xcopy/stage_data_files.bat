e2e888c22ad05a25fd637c861f96e615ac3e7822
JB <rapoly100@gmail.com>
2018-10-08 11:32:15 -0400

    Xcopy update

---
+++ xcopy/stage_data_files.bat
@@ -0,0 +1,9 @@
+
+xcopy /f /s /Y "J:\Global\BI Reporting\Finance\FIS_Bobj\Access\Commencement.accdb" D:\Pentaho\Kettle\Access\
+
+
+ 
+xcopy /f /s /Y "J:\Global\BI Reporting\Finance\FIS_Bobj\FIS_Contract_Feed.csv" D:\Pentaho\Kettle\Dashboard\CSV\
+
+xcopy /f /s /Y  D:\Pentaho\Kettle\Dashboard\HTML\dashboard.html  "J:\Global\BI Reporting\Finance\FIS_Bobj\XML\"  
+xcopy /f /s /Y  D:\Pentaho\Kettle\Dashboard\XML\bobj_report3.xml  "J:\Global\BI Reporting\Finance\FIS_Bobj\XML\" 
